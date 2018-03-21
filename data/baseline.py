04df37c90b14f1186b9612fda1c443142c614a9d
hiwenxin <hiwenxin@umich.edu>
2018-03-20 20:13:55 -0400

    baseline_2

--- data/baseline.py
+++ data/baseline.py
@@ -11,11 +11,41 @@ from nltk.corpus import stopwords
 from numba import jit
 import matplotlib.pyplot as plt
 
+tweets=[]
+emojis=[]
+
 def load_data(filename):
-    pass
+    #load date into two list, tweets and emojis
+    global tweets
+    global emojis
+    f = open(filename, "r", encoding="utf8")
+    rows =f.read().split("\n")
+    for each in rows:
+        row=each.split("\t")
+        if len(row)==2:
+            tweet=row[0]
+            tweets.append(tweet)
+            emoji=row[1]
+            emojis.append(emoji)
+
+def calculate_most_frequent_emoji(emojis):
+    print(emojis)
+    emoji_dict={}
+    for emoji in emojis:
+        if emoji not in emoji_dict:
+            emoji_dict[emoji]=1
+        else:
+            emoji_dict[emoji]+=1
+    #print（sorted(emoji_dict(), key=lambda d: d[1])）
+    #print(emoji_dict)
+
+
 
 def train_data():
     pass
 
 def predict():
     pass
+
+load_data("20_train")
+calculate_most_frequent_emoji(emojis)
